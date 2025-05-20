ce72fc9a8a974ef1826e9768484977fade63a97d
Isis Macedo <89617847+isismodd@users.noreply.github.com>
2025-05-20 20:24:33 -0300

    Update Area_atuacao.java

--- Challenge/src/br/com/fiap/bean/Area_atuacao.java
+++ Challenge/src/br/com/fiap/bean/Area_atuacao.java
@@ -1,5 +1,30 @@
 package br.com.fiap.bean;
 
-public enum AreaAtuacao {
-    CARDIOLOGIA, ORTOPEDIA, PEDIATRIA, CLINICO_GERAL, CIRURGIAO, DERMATOLOGIA, NEUROLOGIA
+public class AreaAtuacao {
+    // Constantes que substituem os valores do enum
+    public static final AreaAtuacao CARDIOLOGIA = new AreaAtuacao("Cardiologia");
+    public static final AreaAtuacao ORTOPEDIA = new AreaAtuacao("Ortopedia");
+    public static final AreaAtuacao PEDIATRIA = new AreaAtuacao("Pediatria");
+    // ... adicione as outras áreas
+
+    private String nome;
+
+    // Construtor privado para evitar novas instâncias
+    private AreaAtuacao(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    // Método para obter todas as áreas disponíveis
+    public static AreaAtuacao[] values() {
+        return new AreaAtuacao[] {
+            CARDIOLOGIA,
+            ORTOPEDIA,
+            PEDIATRIA,
+            // ... outras áreas
+        };
+    }
 }

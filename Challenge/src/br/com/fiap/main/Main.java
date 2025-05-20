19d9bf827ffd2028a76a9cba1514e1f6c10e5833
Isis Macedo <89617847+isismodd@users.noreply.github.com>
2025-05-20 20:29:16 -0300

    Update Main.java

--- Challenge/src/br/com/fiap/main/Main.java
+++ Challenge/src/br/com/fiap/main/Main.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         // Criando hospital
-        Hospital hospital = new Hospital("Hospital das clinicas central");
+        Hospital hospital = new Hospital("Hospital das Clínicas Central");
 
         // Criando unidades
         Unidade unidade1 = new Unidade("Unidade Centro", "Rua Principal, 100");
@@ -14,12 +14,21 @@ public class Main {
         hospital.adicionarUnidade(unidade1);
         hospital.adicionarUnidade(unidade2);
 
-        // Criando médicos
-        Medico cirurgiao = new Medico("Dr. Silva", "CRM/SP 12345", AreaAtuacao.CIRURGIAO);
-        Medico cardiologista = new Medico("Dra. Oliveira", "CRM/SP 54321", AreaAtuacao.CARDIOLOGIA);
+        // Criando médicos usando a classe AreaAtuacao
+        Medico cirurgiao = new Medico(
+            "Dr. Silva", 
+            "CRM/SP 12345", 
+            AreaAtuacao.CIRURGIAO  /
+        );
+        
+        Medico cardiologista = new Medico(
+            "Dra. Oliveira", 
+            "CRM/SP 54321", 
+            AreaAtuacao.CARDIOLOGIA 
+        );
 
         unidade1.adicionarMedico(cirurgiao);
-        unidade2.adicionarMedico(cardiologista);
+        unidade2.adicionarMedico(cardioloista);
 
         // Criando aplicativo
         Aplicativo app = new Aplicativo(hospital);
@@ -30,27 +39,43 @@ public class Main {
         app.cadastrarPaciente(paciente1);
         app.cadastrarPaciente(paciente2);
 
-        // Agendando consultas
+        
         Date hoje = new Date();
         Consulta consulta1 = app.agendarConsulta(paciente1, cirurgiao, hoje);
         consulta1.setDiagnostico("Apendicite aguda - necessária cirurgia");
 
-        // Emitindo receita
+
         Receita receita1 = app.emitirReceita(paciente1, cirurgiao, hoje,
                 "Dipirona 1g - 8/8h por 3 dias\n"
-                        + "Cetoprofeno 100mg - 12/12h por 5 dias\n"
-                        + "Repouso por 7 dias");
+                + "Cetoprofeno 100mg - 12/12h por 5 dias\n"
+                + "Repouso por 7 dias");
 
-        // Exibindo informações
+    
         System.out.println("=== SISTEMA HOSPITALAR ===");
         System.out.println("Hospital: " + hospital.getNome());
+        
+        System.out.println("\nMédicos Cadastrados:");
+        for (Unidade unidade : hospital.getUnidades()) {
+            for (Medico medico : unidade.getMedicos()) {
+                System.out.println("- " + medico.getNome() + 
+                    " (" + medico.getEspecialidade().getNome() + ")");
+            }
+        }
+
         System.out.println("\nPaciente: " + paciente1.getNome());
         System.out.println("Prontuário: " + paciente1.getNumero());
         System.out.println("Última consulta: " + consulta1.getData());
         System.out.println("Médico: " + consulta1.getMedico().getNome() +
-                " (" + consulta1.getMedico().getEspecialidade() + ")");
+                " (" + consulta1.getMedico().getEspecialidade().getNome() + ")");
         System.out.println("Diagnóstico: " + consulta1.getDiagnostico());
+        
         System.out.println("\nReceita médica:");
         System.out.println(receita1.getDescricao());
+
+       
+        System.out.println("\nÁreas de Atuação Disponíveis:");
+        for (AreaAtuacao area : AreaAtuacao.values()) {
+            System.out.println("- " + area.getNome());
+        }
     }
 }
